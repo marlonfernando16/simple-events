c8d0f1d5a6d05dd477ee3cc54d82d711206abbef
Tales Medeiros <t4les313@gmail.com>
2019-04-18 15:14:41 -0300

    primeiro commit

---
+++ src/main/java/config/AppWebConfiguration.java
@@ -0,0 +1,20 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@ComponentScan(basePackages = {"controller"})
+public class AppWebConfiguration {
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/view/");
+		resolver.setSuffix(".jsp");
+		resolver.setExposeContextBeansAsAttributes(true);
+		return resolver;
+	}
+}
