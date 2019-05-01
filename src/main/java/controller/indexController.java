2b8ccac1696cd39b164f14ff72c154054e59ff90
Marlon Five <marlon.fernando16@outlook.com>
2019-05-01 19:05:31 -0300

    testing upload to heroku

---
+++ src/main/java/controller/indexController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class indexController {
+    @GetMapping("/")
+    public String index() {
+        return "Just do it.";
+    }
+}
